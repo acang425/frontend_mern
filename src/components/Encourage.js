63f5985f32b88aef2a40125c3c85e130bd3b6c5e
acang425 <117882062+acang425@users.noreply.github.com>
2023-12-28 02:04:59 +0800

    Update Encourage.js

--- src/components/Encourage.js
+++ src/components/Encourage.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef,useEffect } from 'react';
 import { ConfigProvider, List, Image, Button, Icon } from 'zarm';
 import zhCN from 'zarm/lib/config-provider/locale/zh_CN';
 import 'zarm/dist/zarm.css';
@@ -13,27 +13,20 @@ import money from './images/money.png'
 import ListItem from 'zarm/lib/list/ListItem';
 
 import root from './Approot';
-
-const data = [
-  {
-    id: 1,
-    name: '用户A',
-    bio: '我是用户A',
-    coins: 100,
-    avatar: 'https://img.yzcdn.cn/vant/cat.jpeg',
-  },
-  {
-    id: 2,
-    name: '用户B',
-    bio: '我是用户B',
-    coins: 90,
-    avatar: 'https://img.yzcdn.cn/vant/dog.jpeg',
-  },
-  // ... 其他用户的数据
-];
+import axios from 'axios';
 
 
+
+function MyEncourage() {
+
+  const[usernames,setUsernames]=useState([]);
+  const[vprice,setvprice]=useState([]);
+  const[size,setSize]=useState();
+
+  
 function example(i) {
+  
+
   return [
     <div style={{ width: '100%', color: '#666666' }}>
       <div style={{ width: '100%' }}>
@@ -46,27 +39,47 @@ function example(i) {
         <div style={{ paddingLeft: '15px', width: '40%', float: 'left' }}>
           <div>
             <span style={{ fontSize: '25px', fontWeight: '600' }}>
-              <span style={{ marginRight: '15px' }}>Windir</span>
+              <span style={{ marginRight: '15px' }}>{usernames[i-1]}</span>
             </span>
           </div>
         </div>
         <div style={{ paddingLeft: '50px', width: '20%', float: 'left' }}>
           <List>
-            <ListItem><span style={{ fontSize: '30px' }}>11000</span><img style={{ margin: '10px', width: '30px' }} src={money}></img></ListItem>
+            <ListItem><span style={{ fontSize: '30px' }}>{vprice[i-1]}</span><img style={{ margin: '10px', width: '30px' }} src={money}></img></ListItem>
           </List>
         </div>
       </div>
       <div style={{ width: '100%' }}>
-        <p style={{ marginLeft:'20px',margin: '50px', fontSize: '20px', color: '#333435' }}>奋斗正当时</p>
+        <p style={{ marginLeft: '20px', margin: '50px', fontSize: '20px', color: '#333435' }}>奋斗正当时</p>
       </div>
     </div>
   ]
 }
 
-function MyEncourage() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/user/list'); // 发起GET请求获取数据
+        console.log(response.data.data);
+        const data=response.data.data.list;
+        const usernames = data.map(item => item.user_name);
+        const vp = data.map(item => item.v_price);
+        setUsernames(usernames)
+        console.log(usernames);
+        setvprice(vp)
+        setSize(response.data.data.count)
+        // setData(response.data); // 将获取的数据存储在状态中
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData(); // 在组件挂载后立即获取数据
+  }, []);
+
   const containerRef = useRef();
   const [userRank, setUserRank] = useState(2); // 用户的排名
-  const numbers = Array.from(20, (_, i) => i + 1);
+  const numbers = Array.from(3, (_, i) => i + 1);
   return (
     <div className="encourage-rec">
       <ConfigProvider locale={zhCN}>
@@ -87,19 +100,19 @@ function MyEncourage() {
             style={{
               overflowY: 'auto',
               maxHeight: '1075px',
-              background:'#F5F5F5'
+              background: '#F5F5F5'
             }}
           >
             <div>
               <img style={{ width: '100%' }} src={u16}></img>
             </div>
-            <div style={{marginBottom:'50px'}}>
+            <div style={{ marginBottom: '50px' }}>
               <List>
                 <ListItem>
                   <div style={{ width: '100%', color: '#666666' }}>
                     <div style={{ width: '100%' }}>
                       <div style={{ fontSize: '30px', width: '10%', float: 'left' }}>
-                        10
+                        1
                       </div>
                       <div style={{ width: '20%', float: 'left' }}>
                         <img style={{ height: '80px', width: '80px' }} src={u17}></img>
@@ -107,7 +120,7 @@ function MyEncourage() {
                       <div style={{ paddingLeft: '15px', width: '40%', float: 'left' }}>
                         <div>
                           <span style={{ fontSize: '25px', fontWeight: '600' }}>
-                            <span style={{ marginRight: '15px' }}>{localStorage.getItem('name')==null?"Windir":localStorage.getItem('name')}</span>
+                            <span style={{ marginRight: '15px' }}>{localStorage.getItem('name') == null ? "Windir" : localStorage.getItem('name')}</span>
                           </span>
                         </div>
                       </div>
@@ -127,7 +140,7 @@ function MyEncourage() {
             </div>
             <div>
               <List>
-                {[...Array(20)].map((_, index) => (
+                {[...Array(size)].map((_, index) => (
                   <ListItem key={index}>
                     <div>{example(index + 1)}</div>
                   </ListItem>
